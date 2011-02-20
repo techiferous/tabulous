c89169a521ae5ffbf8fcc13274a91136c81678aa
Wyatt Greene <techiferous@gmail.com>
2011-02-20 11:48:23 -0500

    Added test app simple_tabs.

---
+++ test/applications/simple_tabs/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleTabs::Application.config.secret_token = '03927c62defd8c018b09a1bf84dc000c3607f0458c57e5f91e9e404ae8db4dd21d7ada053fc2ea39d158cb201cd1e46f6375c0c6af1e5c7463ea049a6d5522f4'
