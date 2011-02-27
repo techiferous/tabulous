135aa917103995f293a8624444131948d3b850a4
Wyatt Greene <techiferous@gmail.com>
2011-02-27 13:19:34 -0500

    Reload tabulous initializer on every request if in development mode.

--- lib/tabulous/railtie.rb
+++ lib/tabulous/railtie.rb
@@ -3,11 +3,21 @@ require 'rails'
 
 module Tabulous
   class Railtie < Rails::Railtie
-    initializer 'tabulous.helper' do |app|  
-      ActionView::Base.send :include, Tabulous::Helpers
-    end  
     rake_tasks do
       load "tasks/tabulous.rake"
+    end
+    initializer 'tabulous.helper' do |app|  
+      ActionView::Base.send :include, Tabulous::Helpers
+    end
+    initializer 'tabulous.reload' do |app|
+      ActiveSupport.on_load(:action_controller) do
+        if Rails.env.development?
+          before_filter do
+            # reload tabulous initializer on every request if in development mode
+            load File.join(Rails.root.to_s, 'config/initializers/tabulous.rb')
+          end
+        end
+      end
     end
   end
 end
