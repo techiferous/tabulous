eae04e1058a809f6395579d51322eacd47ecf725
Wyatt Greene <techiferous@gmail.com>
2011-02-26 23:48:48 -0500

    Refactored formatter class.

--- lib/tabulous/tabulous_formatter.rb
+++ lib/tabulous/tabulous_formatter.rb
@@ -10,70 +10,81 @@ class TabulousFormatter
 
   def self.format(lines)
     @out = []
-    inside = false
-    inside_lines = []
+    inside_a_table = false
+    table_lines = []
     headings = []
     indentation = nil
     lines.each do |line|
-      line = line.rstrip
-      if inside && end_line?(line)
-        output_formatted_table(indentation, headings, inside_lines)
-        inside = false
-        inside_lines = []
+      line.rstrip!
+      if inside_a_table && at_end_of_table?(line)
+        prepare_formatted_table(indentation, headings, table_lines)
+        inside_a_table = false
+        table_lines = []
         headings = []
         indentation = nil
       end
-
-      if inside
+      if inside_a_table
         stripped_line = line.strip
-        if indentation.nil? && line =~ /^(\s*)#--/
+        at_header_labels = headings.empty? && stripped_line.starts_with?('# ') && stripped_line.slice('|')
+        at_table_row = stripped_line.starts_with?('[')
+        at_start_of_header = (indentation.nil? && line =~ /^(\s*)#--/)
+        if at_start_of_header
           indentation = $1
-        elsif headings.empty? && stripped_line.starts_with?('# ') && stripped_line.slice('|')
-          headings = stripped_line.split(%r{(#|\|)}).map(&:strip)
-          headings.reject!{|x| x.empty? || x == '#' || x == '|'}
-        elsif stripped_line.starts_with?('[')
+        elsif at_header_labels
+          headings = parse_header_labels(stripped_line)
+        elsif at_table_row
           if stripped_line =~ /^\[(.+)\],(\s*|\s*#.*)$/
             cells = $1
             end_of_line_comment = $2
-            inside_lines << cells.split(' , ').map(&:strip) + [end_of_line_comment]
+            table_lines << cells.split(' , ').map(&:strip) + [end_of_line_comment]
           end
         end
       else
-        output line
+        @out << line
       end
-
-      if !inside && start_line?(line)
-        inside = true
+      if !inside_a_table && at_beginning_of_table?(line)
+        inside_a_table = true
       end
     end
     return @out
   end
 
-  def self.output(line)
-    @out << line
+  def self.parse_header_labels(line)
+    headings = line.split('|')
+    first_heading = headings.shift
+    last_heading = headings.pop
+    first_heading = first_heading.split('#').last.strip
+    if last_heading =~ /#\s*\S+/
+      raise "no text can appear after table headings. aborting" # TODO: better error message
+    end
+    last_heading = last_heading.split('#').first.strip
+    headings = [first_heading] + headings.map(&:strip) + [last_heading]
+    headings
   end
-  
-  def self.start_line?(line)
+  private_class_method :parse_header_labels
+
+  def self.at_beginning_of_table?(line)
     line = line.split('#').first # naively strip out comments
     return false if line.nil?
     line = line.gsub(/\s/, '')
     return line == 'config.tabs=[' || line == 'config.actions=['
   end
+  private_class_method :at_beginning_of_table?
   
-  def self.end_line?(line)
+  def self.at_end_of_table?(line)
     line = line.split('#').first # naively strip out comments
     return false if line.nil?
     line = line.gsub(/\s/, '')
     return line == ']'
   end
-  
-  def self.output_formatted_table(indentation, headings, inside_lines)
+  private_class_method :at_end_of_table?
 
+  def self.prepare_formatted_table(indentation, headings, table_lines)
     num_columns = headings.size
     column_sizes = {}
     for column in (0..num_columns-1)
       cells = [headings[column]]
-      cells += inside_lines.map{|x| x[column]}
+      cells += table_lines.map{|x| x[column]}
       column_sizes[column] = cells.map(&:length).max
     end
     padding = ' ' * 4
@@ -86,11 +97,10 @@ class TabulousFormatter
     header = headings.join('|')
     header_divider = indentation + '#' + ('-' * header.size) + '#'
     header = indentation + '#' + header + '#'
-    output header_divider
-    output header
-    output header_divider
-    
-    for cells in inside_lines
+    @out << header_divider
+    @out << header
+    @out << header_divider
+    for cells in table_lines
       for column in (0..num_columns-1)
         cells[column] = padding + cells[column].ljust(column_sizes[column]) + padding
       end
@@ -100,12 +110,12 @@ class TabulousFormatter
       unless end_of_line_comment.blank?
         line += '  ' + end_of_line_comment.strip
       end
-      output line
+      @out << line
     end
-
-    output header_divider
-    output header
-    output header_divider
+    @out << header_divider
+    @out << header
+    @out << header_divider
   end
+  private_class_method :prepare_formatted_table
 
 end
