0ad2a5154272a5bf5e8451679d494351aefd90db
Wyatt Greene <techiferous@gmail.com>
2011-02-26 20:24:49 -0500

    Got first basic test for tabulous_formatter passing.

--- test/unit_test_helper.rb
+++ test/unit_test_helper.rb
@@ -1,4 +1,4 @@
+$:.unshift(File.expand_path(File.dirname(__FILE__) + "/../lib"))
 require 'rubygems'
 require 'test/unit'
 require 'redgreen'
-#$:.unshift(File.expand_path(File.dirname(__FILE__) + "/../lib"))
--- test/units/tabulous_formatter_test.rb
+++ test/units/tabulous_formatter_test.rb
@@ -1,9 +1,14 @@
 require File.expand_path('../unit_test_helper', File.dirname(__FILE__))
+require 'tabulous/tabulous_formatter'
 
 class TabulousFormatterTest < Test::Unit::TestCase
 
-  def test_truth
-    assert true
+  def test_returns_array_of_strings
+    result = TabulousFormatter.format(['foo', 'bar'])
+    assert_kind_of Array, result
+    for element in result
+      assert_kind_of String, element
+    end
   end
 
 end
