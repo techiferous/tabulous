393fc3cdcde41a9bc1d1c03dc1daa8abd1787089
Wyatt Greene <techiferous@gmail.com>
2011-02-27 12:26:51 -0500

    Removed hover tabs test application.

--- test/applications/hover_tabs/test/integration_test_helper.rb
+++
@@ -1,6 +0,0 @@
-require File.expand_path('test_helper', File.dirname(__FILE__))
-require File.expand_path('../../../test_application_integration_test_helper', File.dirname(__FILE__))
-
-# This file contains integration test code specific to this test application.  Integration
-# test code that is shared among all test applications should live in
-# test/test_application_integration_test_helper.rb.
