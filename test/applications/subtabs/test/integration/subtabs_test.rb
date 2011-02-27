17b105d3050d5eae8ff7cc5dc0b58c0e75a3fbc2
Wyatt Greene <techiferous@gmail.com>
2011-02-27 11:24:24 -0500

    Added tab visibility integration tests.

--- test/applications/subtabs/test/integration/subtabs_test.rb
+++ test/applications/subtabs/test/integration/subtabs_test.rb
@@ -2,9 +2,43 @@ require File.expand_path("../integration_test_helper", File.dirname(__FILE__))
  
 class SubtabsTest < ActionController::IntegrationTest
  
-  test "viewing subtabs" do
+  test "viewing subtabs application" do
     visit '/'
     assert page.has_content?('Explanation')
+  end
+ 
+  test "displaying subtabs" do
+    visit '/'
+    click_link 'Galaxies'
+    assert page.has_content?('Elliptical Galaxies')
+    assert page.has_content?('Spiral Galaxies')
+    assert page.has_content?('Lenticular Galaxies')
+  end
+ 
+  test "selecting a subtab" do
+    visit '/'
+    click_link 'Galaxies'
+    assert page.has_content?('Listing elliptical_galaxies')
+    click_link 'Spiral Galaxies'
+    assert page.has_content?('Listing spiral_galaxies')
+    click_link 'Lenticular Galaxies'
+    assert page.has_content?('Listing lenticular_galaxies')
+  end
+ 
+  test "tab visibility" do
+    visit '/'
+    assert_tab_not_present 'Hidden'
+    click_link 'Galaxies'
+    assert_tab_present 'Hidden'
+    click_link 'Planets'
+    assert_tab_not_present 'Hidden'
+  end
+ 
+  test "subtab visibility" do
+    click_link 'Galaxies'
+    click_link 'Hidden'
+    assert_subtab_present 'Always Visible'
+    assert_subtab_not_present 'Always Hidden'
   end
  
 end
--- test/test_application_integration_test_helper.rb
+++ test/test_application_integration_test_helper.rb
@@ -3,5 +3,46 @@ require 'capybara/rails'
 module ActionController
   class IntegrationTest
     include Capybara
+
+    def assert_tab_not_present(text)
+      begin
+        message = "expected tab with text '#{text}' to not be present"
+        page.find(:xpath, %Q{//div[@id="tabs"]//ul/li/*[. = "#{text}"]})
+        assert_block(message) { false }
+      rescue Capybara::ElementNotFound:
+        assert_block(message) { true }
+      end
+    end
+
+    def assert_tab_present(text)
+      begin
+        message = "expected tab with text '#{text}' to be present"
+        page.find(:xpath, %Q{//div[@id="tabs"]//ul/li/*[. = "#{text}"]})
+        assert_block(message) { true }
+      rescue Capybara::ElementNotFound:
+        assert_block(message) { false }
+      end
+    end
+
+    def assert_subtab_not_present(text)
+      begin
+        message = "expected subtab with text '#{text}' to not be present"
+        page.find(:xpath, %Q{//div[@id="subtabs"]//ul/li/*[. = "#{text}"]})
+        assert_block(message) { false }
+      rescue Capybara::ElementNotFound:
+        assert_block(message) { true }
+      end
+    end
+
+    def assert_subtab_present(text)
+      begin
+        message = "expected subtab with text '#{text}' to be present"
+        page.find(:xpath, %Q{//div[@id="subtabs"]//ul/li/*[. = "#{text}"]})
+        assert_block(message) { true }
+      rescue Capybara::ElementNotFound:
+        assert_block(message) { false }
+      end
+    end
+
   end
 end
