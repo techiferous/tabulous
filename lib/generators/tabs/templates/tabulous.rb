dd2102165c54fae0b571ed2eb34ad6d8653f12ca
Wyatt Greene <techiferous@gmail.com>
2011-02-28 20:03:31 -0500

    Added options to tabs generator template.  Also made generated tables line up.

--- lib/generators/tabs/templates/tabulous.rb
+++ lib/generators/tabs/templates/tabulous.rb
@@ -11,30 +11,46 @@
 -%>
 Tabulous.setup do |config|
 
-  config.tabs = [
-    #-------------------------------------------------------------------------------------------------#
-    #    TAB NAME         |    DISPLAY TEXT     |    PATH           |    VISIBLE?    |    ENABLED?    #
-    #-------------------------------------------------------------------------------------------------#
-<% for c in controllers -%>
-    [    :<%= c %>_tab  ,  '<%= c.titleize %>'  ,  '/<%= c %>'  ,  true  ,  true  ],
-<% end -%>
-    #-------------------------------------------------------------------------------------------------#
-    #    TAB NAME         |    DISPLAY TEXT     |    PATH           |    VISIBLE?    |    ENABLED?    #
-    #-------------------------------------------------------------------------------------------------#
-  ]
+<%= s = []
+    s <<  '  config.tabs = ['
+    s <<  '    #-------------------------------------------------------------------------------------------------#'
+    s <<  '    #    TAB NAME         |    DISPLAY TEXT     |    PATH           |    VISIBLE?    |    ENABLED?    #'
+    s <<  '    #-------------------------------------------------------------------------------------------------#'
+    for c in controllers 
+      s << "[    :#{c}_tab  ,  '#{c.titleize}'  ,  '/#{c}'  ,  true  ,  true  ],"
+    end
+    s <<  '    #-------------------------------------------------------------------------------------------------#'
+    s <<  '    #    TAB NAME         |    DISPLAY TEXT     |    PATH           |    VISIBLE?    |    ENABLED?    #'
+    s <<  '    #-------------------------------------------------------------------------------------------------#'
+    s <<  '  ]'
+    Tabulous::Formatter.format(s).join("\n") %>
 
-  config.actions = [
-    #-------------------------------------------------------------#
-    #    CONTROLLER    |    ACTION          |    TAB              #
-    #-------------------------------------------------------------#
-<% for c in controllers -%>
-    [    :<%= c %>  ,  :all_actions  ,  :<%= c %>_tab  ],
-<% end -%>
-    #-------------------------------------------------------------#
-    #    CONTROLLER    |    ACTION          |    TAB              #
-    #-------------------------------------------------------------#
-  ]
+<%= s = []
+    s <<  '  config.actions = ['
+    s <<  '    #-------------------------------------------------------------#'
+    s <<  '    #    CONTROLLER    |    ACTION          |    TAB              #'
+    s <<  '    #-------------------------------------------------------------#'
+    for c in controllers 
+      s << "[    :#{c}  ,  :all_actions  ,  :#{c}_tab  ],"
+    end
+    s <<  '    #-------------------------------------------------------------#'
+    s <<  '    #    CONTROLLER    |    ACTION          |    TAB              #'
+    s <<  '    #-------------------------------------------------------------#'
+    s <<  '  ]'
+    Tabulous::Formatter.format(s).join("\n") %>
   
+  # config.active_tab_clickable = true
+  # config.always_render_subtabs = true
+  # config.raise_error_if_no_tab_found = false
+  # config.html5 = true
+
   config.css.scaffolding = true
+  # config.css.background_color = '#ccc'
+  # config.css.text_color = '#444'
+  # config.css.active_tab_color = 'white'
+  # config.css.hover_tab_color = '#ddd'
+  # config.css.inactive_tab_color = '#aaa'
+  # config.css.inactive_text_color = '#888'
+
 
 end
