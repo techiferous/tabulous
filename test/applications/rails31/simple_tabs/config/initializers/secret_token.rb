fc6cf78174601645af8ee1b05832efcf7fbe606a
Wyatt Greene <techiferous@gmail.com>
2011-11-06 17:09:53 -0500

    Added a Rails 3.1 version of the simple_tabs test application.

---
+++ test/applications/rails31/simple_tabs/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleTabs::Application.config.secret_token = 'df67a5433adf3be925d05b9d785fcc77bdebde4037fbbfc97b43e31bb77fbe27d1e1f0b84548e4789bd6109af7163c6b8962bd75e58d4ad9a6950f507beea670'
--- test/test_application_gemfile.rb
+++ test/test_application_gemfile.rb
@@ -6,6 +6,13 @@ def shared_test_application_gems(bundler, rails_version)
       gem 'rails', '~>3.0.0'
     elsif rails_version == '3.1'
       gem 'rails', '~>3.1.0'
+      group :assets do
+        gem 'sass-rails',   '~> 3.1.4'
+        gem 'coffee-rails', '~> 3.1.1'
+        gem 'uglifier', '>= 1.0.3'
+      end
+      gem 'jquery-rails'
+      gem 'sqlite3', '1.3.4'
     else
       raise "Unknown rails version '#{rails_version}'."
     end
@@ -15,6 +22,10 @@ def shared_test_application_gems(bundler, rails_version)
       gem 'capybara'
       gem 'launchy'
       gem 'redgreen' unless RUBY_VERSION >= "1.9"
+      if rails_version == '3.1'
+        gem 'turn', :require => false
+        gem 'minitest'
+      end
     end
   end
 end
