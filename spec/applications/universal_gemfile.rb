8903c1e825ea45cc399d5a9dca2bdef7a2a8bf49
Wyatt Greene <techiferous@gmail.com>
2016-08-06 14:23:46 -0400

    Add support for Rails 5.0.

--- spec/applications/universal_gemfile.rb
+++ spec/applications/universal_gemfile.rb
@@ -71,6 +71,28 @@ def create_gemfile
         gem 'spring'
       end
     CODE
+  elsif rails_version? '5.0'
+    gemfile += <<-CODE.strip_heredoc
+      gem 'rails', '~> 5.0.0'
+      gem 'puma', '~> 3.0'
+      gem 'sass-rails', '~> 5.0'
+      gem 'bootstrap-sass', '~> 3.0.2.0'
+      gem 'uglifier', '>= 1.3.0'
+      gem 'coffee-rails', '~> 4.2'
+      gem 'jquery-rails'
+      gem 'turbolinks', '~> 5'
+      gem 'jbuilder', '~> 2.5'
+      group :development, :test do
+        gem 'byebug', platform: :mri
+      end
+      group :development do
+        gem 'web-console'
+        gem 'listen', '~> 3.0.5'
+        gem 'spring'
+        gem 'spring-watcher-listen', '~> 2.0.0'
+      end
+      gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+    CODE
   end
 
   add_file 'Gemfile', gemfile
