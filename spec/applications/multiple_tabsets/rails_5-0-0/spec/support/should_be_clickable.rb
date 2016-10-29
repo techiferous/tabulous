a2a7d4083b6d50981890c71d2fb6f9768493c4cc
Fabian Winkler <wynk.spam@quetschwalze.org>
2016-10-29 15:15:46 +0200

    Use failure_message instead of deprecated failure_message_for_should And replace failure_message_for_should_not with failure_message_when_negated.

--- spec/applications/multiple_tabsets/rails_5-0-0/spec/support/should_be_clickable.rb
+++ spec/applications/multiple_tabsets/rails_5-0-0/spec/support/should_be_clickable.rb
@@ -5,7 +5,7 @@ RSpec::Matchers.define :be_clickable do
     !clickable_tab.empty? || !clickable_subtab.empty?
   end
 
-  failure_message_for_should do |actual|
+  failure_message do |actual|
     "expected that a tab or subtab with the text '#{tab_text}' would be clickable"
   end
 
